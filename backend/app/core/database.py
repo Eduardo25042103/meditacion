ccbe77ce13816cc0d1456148ca16c66dab86ed9d
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-04-30 15:51:21 +0000

    database.py// Se crea motor, sesion y dependencias. #13 bakend// Se inicializa alembic

---
+++ backend/app/core/database.py
@@ -0,0 +1,35 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.pool import NullPool
+
+#Configuración bd
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+#Motor async
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    poolclass=NullPool,
+)
+
+#Sesion async
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
+
+Base = declarative_base()
+
+#Dependency para FastAPI
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        
+        finally:
+            await session.close()
+
