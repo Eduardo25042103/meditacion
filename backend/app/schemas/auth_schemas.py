f9b2c5b6ea817cae53e5b0a473c8e1f1eaedd480
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-05-05 01:45:28 +0000

    backend.app.schemas.auth_schemas.py // Se crean esquemas UserLogin y Token

--- backend/app/schemas/auth_schemas.py
+++ backend/app/schemas/auth_schemas.py
@@ -5,3 +5,11 @@ class UserCreate(BaseModel):
     email: EmailStr = Field(..., example="user@gmail.com")
     password: str = Field(..., min_length=6, example="Pass_3xampl3")
 
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
